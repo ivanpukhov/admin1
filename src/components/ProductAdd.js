bb1d28fdbb8dbbe82a88b727fbd9d15626818102
root <root@jdyzcepdyx.local>
2024-09-19 12:32:23 +0000

    d

--- src/components/ProductAdd.js
+++ src/components/ProductAdd.js
@@ -1,8 +1,8 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-// Модальное окно
 function Modal({ isOpen, message, onClose }) {
     if (!isOpen) return null;
 
@@ -15,7 +15,6 @@ function Modal({ isOpen, message, onClose }) {
         </div>
     );
 }
-
 
 function ProductAdd() {
     const [categories, setCategories] = useState([]);
@@ -33,9 +32,15 @@ function ProductAdd() {
     const [modalMessage, setModalMessage] = useState("");
 
     useEffect(() => {
-        axios.get('/api/products/categories').then(response => {
-            setCategories(response.data.map(item => item.category));
-        });
+        axios.get('/api/products/categories')
+            .then(response => {
+                setCategories(response.data.map(item => item.category));
+            })
+            .catch(error => {
+                console.error('Error fetching categories:', error);
+                setModalMessage('Failed to fetch categories');
+                setIsModalOpen(true);
+            });
     }, []);
 
     const handleChange = (e) => {
@@ -90,7 +95,10 @@ function ProductAdd() {
             setIsModalOpen(true);
         } catch (error) {
             console.error('Error during product addition:', error);
-            setModalMessage('Failed to add product');
+            const errorMessage = error.response && error.response.data && error.response.data.error
+                ? error.response.data.error
+                : 'Failed to add product';
+            setModalMessage(errorMessage);
             setIsModalOpen(true);
         }
     };
@@ -99,19 +107,16 @@ function ProductAdd() {
         navigate(-1);
     };
 
-
     return (
         <div className="product-add">
             <div className="ordr">
                 <div className='detail__back' onClick={handleGoBack}>
                     <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect width="44" height="44" rx="10" fill="#C6ECE8"/>
-                        <path d="M26.2188 13.5625L17.7812 22L26.2188 30.4375" stroke="#0CE3CB" strokeWidth="2.5"
-                              strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M26.2188 13.5625L17.7812 22L26.2188 30.4375" stroke="#0CE3CB" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                 </div>
                 <h2>Добавить товар</h2>
-
             </div>
             <form className="checkout__user" onSubmit={handleSubmit}>
                 <label className="checkout__input">
@@ -120,12 +125,10 @@ function ProductAdd() {
                 </label>
                 <label className="checkout__input">
                     <div>Описание</div>
-
                     <input className="checkout__input-item" type="text" name="description" value={productData.description} onChange={handleChange} required />
                 </label>
                 <label className="checkout__input">
                     <div>Цена</div>
-
                     <input className="checkout__input-item" type="number" name="price" value={productData.price} onChange={handleChange} required />
                 </label>
                 <label className="checkout__input">
@@ -141,7 +144,7 @@ function ProductAdd() {
                         required
                     />
                     {showSuggestions && (
-                        <ul className="order__add" >
+                        <ul className="order__add">
                             {categories.filter(cat => cat.includes(productData.category)).map((category, index) => (
                                 <li key={index} onClick={() => handleCategoryClick(category)}>
                                     {category}
@@ -152,17 +155,15 @@ function ProductAdd() {
                 </label>
                 <label className="checkout__input">
                     <div>Подкатегория</div>
-
                     <input className="checkout__input-item" type="text" name="subcategory" value={productData.subcategory} onChange={handleChange} required />
                 </label>
                 <label className="checkout__input">
                     <div>Изображение</div>
-                    <input type="file" onChange={handleFileChange} />
+                    <input type="file" onChange={handleFileChange} required />
                 </label>
                 <button className="checkout__btn" type="submit">Создать продукт</button>
             </form>
             <Modal isOpen={isModalOpen} message={modalMessage} onClose={() => setIsModalOpen(false)} />
-
         </div>
     );
 }
